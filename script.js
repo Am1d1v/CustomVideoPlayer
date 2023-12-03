032bb1de3e16ce68503bf997f42c1b35d6a69144
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-12-03 17:24:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 
 
+const video = document.querySelector('#video');
+const play = document.querySelector('#play');
+const stop = document.querySelector('#stop');
+const progress = document.querySelector('#progress');
+const timestamp = document.querySelector('#timestamp');
+
+
+// Play and Pause video
+function toggleVideoStatus(){
+    return true;
+}
+
+// Update Play/Pause icon
+function updatePlayIcon(){
+    return true;
+}
+
+// Update Progress
+function updateProgress(){
+    return true;
+}
+
+// Update progress and timestamp
+function setVideoProgress(){
+    return true;
+}
+
+// Stop video
+function stopVideo(){
+    return true;
+}
+
+
+// Event Lsiteners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress)
